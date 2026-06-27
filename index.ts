4e8634ece97a8ec04602b448fdf52324005400e4
tan <tanmayrajkrishna@gmail.com>
2026-06-27 13:38:55 +0530

    add biome

--- index.ts
+++ index.ts
@@ -1,1 +1,17 @@
-console.log("Hello via Bun!");
+console.log('ok i have no idea what im doing');
+
+const a = 'lol';
+
+interface Track {
+    title: string;
+    duration: number;
+    artist: string;
+}
+
+const song: Track = {
+    title: 'Hand On Me',
+    duration: 231,
+    artist: 'Nourished by Time',
+};
+
+console.log(song);
